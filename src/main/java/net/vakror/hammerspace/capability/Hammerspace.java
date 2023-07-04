5d291e3b4d4f3bd10fe53442fc4aa9fe31b15f97
vakror <vaylor2027@gmail.com>
2023-07-03 22:08:01 -0400

    add new values to Hammerspace capability

--- src/main/java/net/vakror/hammerspace/capability/Hammerspace.java
+++ src/main/java/net/vakror/hammerspace/capability/Hammerspace.java
@@ -7,15 +7,21 @@ import net.vakror.hammerspace.HammerspaceMod;
 
 public class Hammerspace {
     private double gravity = 1;
+    private int tick = 1;
+    private double fluidFlowSpeed = 1;
     public CompoundTag toNbt() {
         CompoundTag nbt = new CompoundTag();
         nbt.putDouble("gravity", gravity);
+        nbt.putInt("tick", tick);
+        nbt.putDouble("fluidFlowSpeed", fluidFlowSpeed);
 
         return nbt;
     }
 
     public void fromNbt(CompoundTag nbt) {
         gravity = nbt.getDouble("gravity");
+        tick = nbt.getInt("tick");
+        fluidFlowSpeed = nbt.getDouble("fluidFlowSpeed");
     }
 
     public double gravity() {
@@ -24,5 +30,21 @@ public class Hammerspace {
 
     public void setGravity(double gravity) {
         this.gravity = gravity;
+    }
+
+    public int tick() {
+        return tick;
+    }
+
+    public void setTick(int tick) {
+        this.tick = tick;
+    }
+
+    public double fluidFlowSpeed() {
+        return fluidFlowSpeed;
+    }
+
+    public void setFluidFlowSpeed(double fluidFlowSpeed) {
+        this.fluidFlowSpeed = fluidFlowSpeed;
     }
 }
